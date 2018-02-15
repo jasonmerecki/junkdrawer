986135f6fc32072a6aa5a67216cbca4f22c2c613
Jason Merecki <jasonmerecki@gmail.com>
2018-02-14 22:15:13 -0700

    two tests are complete including the setinterval to make it async

--- htdocs/jsPerf/reactComponent.js
+++ htdocs/jsPerf/reactComponent.js
@@ -1,17 +1,78 @@
-class HelloMessage extends React.Component {
+
+const state1 = {symbols:
+        {
+            ABC: {symbol: "ABC", price:"49.02" },
+            DEF: {symbol: "DEF", price:"132.18" },
+            GJI: {symbol: "GHI", price:"89.01" }
+        }
+};
+
+const state2 = {symbols:
+        {
+            ABC: {symbol: "ABC", price:"49.71" },
+            DEF: {symbol: "DEF", price:"132.18" },
+            GJI: {symbol: "GHI", price:"89.71" }
+        }
+};
+
+
+const standardHeader = React.createElement(
+    "thead",
+    null,
+    React.createElement("th",{colspan: 2}, "Table contents changed and built by React")
+);
+
+
+class TestRow extends React.Component {
     render() {
-        return React.createElement(
-            "div",
-            null,
-            "Hello ",
-            this.props.name
+        const td1 = React.createElement("td", null, this.props.symbol);
+        const td2 = React.createElement("td", null, this.props.price);
+        return React.createElement("tr", null, td1, td2 );
+    };
+};
+
+class TestBpdy extends React.Component {
+    render() {
+        const childRows = [];
+        for (const symbolObj of Object.values(this.props.symbols)) {
+            childRows.push(React.createElement(TestRow, symbolObj));
+        }
+        return React.createElement("tbody", null, childRows);
+    };
+};
+
+// ugly work-around because the webpage is not 'contained' in a react app
+const tableHolder = {};
+
+class TestTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = state1;
+        tableHolder[this.props.name] = this;
+    };
+    componentDidUpdate(prevProps, prevState) {
+        tableHolder[this.props.name].postUpdate();
+    };
+    render() {
+        const bodyComponent = React.createElement(
+            TestBpdy, this.state
         );
-    }
+        return React.createElement("table", {border: 2}, standardHeader, bodyComponent);
+    };
 }
+
+
 
 const reactEle = document.getElementById('reactComponent');
 
-ReactDOM.render(React.createElement(HelloMessage, { name: "Taylor" }), reactEle);
+const tableElement = React.createElement(TestTable, {name: "mytable"});
+
+ReactDOM.render(tableElement, reactEle);
+
+// tableHolder["mytable"].setState(state2);
+
+
+
 
 /*
 syntax:
@@ -21,4 +82,27 @@ React.createElement(
   [props],
   [...children]
 )
+ */
+
+/*
+The table being built:
+<table border="2">
+        <thead>
+        <th colspan="2">Table contents changed by innerHTML text</th>
+        </thead>
+        <tbody id="changeTableByText">
+        <tr>
+            <td>ABC</td>
+            <td>49.02</td>
+        </tr>
+        <tr>
+            <td>DEF</td>
+            <td>132.18</td>
+        </tr>
+        <tr>
+            <td>GHI</td>
+            <td>89.01</td>
+        </tr>
+        </tbody>
+    </table>
  */
--- htdocs/jsPerf/testScripts.js
+++ htdocs/jsPerf/testScripts.js
@@ -1,20 +1,100 @@
-const statusDesc = document.getElementById('statusDesc');
-
 
 const newDiv = document.createElement("div");
 newDiv.innerText = "Div was inserted here";
 const emptyParagraph = document.getElementById('emptyParagraph');
 emptyParagraph.appendChild(newDiv);
 
+while (emptyParagraph.hasChildNodes()) {
+    emptyParagraph.removeChild(emptyParagraph.lastChild);
+}
+
+let maxIterations = 0, iterations = 0, scheduleTime = 1;
+let firstState = false;
+
+// function and selector for swapping out text
+
+const paraBodyString1 = "<tr><td>ABC</td><td>49.02</td></tr>\n" +
+    "<tr><td>DEF</td><td>132.18</td></tr>\n" +
+    "<tr><td>GHI</td><td>89.01</td></tr>";
+
+const paraBodyString2 = "<tr><td>ABC</td><td>49.71</td></tr>\n" +
+    "<tr><td>DEF</td><td>132.18</td></tr>\n" +
+    "<tr><td>GHI</td><td>89.71</td></tr>";
+
+const changeTableTextEle = document.getElementById('changeTableByText');
+
+let startTextTime;
+let totalTextMills;
+
+const changeTextFunction = function() {
+    const useState = firstState ? paraBodyString1 : paraBodyString2;
+    changeTableTextEle.innerHTML = useState;
+    firstState = !firstState;
+    iterations++;
+    if (iterations < maxIterations) {
+        setTimeout(changeTextFunction, scheduleTime);
+    } else {
+        totalTextMills = Date.now() - startTextTime;
+        finishtest();
+    }
+}
+
+// function and selector for changing elements
+
+const changeTableByElements = document.getElementById('changeTableByElements');
+
+let startEleTime;
+let totalEleMills;
+
+const changeElementsFunction = function() {
+    // remove the current elements
+    while (changeTableByElements.hasChildNodes()) {
+        changeTableByElements.removeChild(changeTableByElements.lastChild);
+    }
+    // create a row for each item
+    const useState = firstState ? state1 : state2;
+    for (const symbolObj of Object.values(useState.symbols)) {
+        const trele = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.textContent = symbolObj.symbol;
+        const td2 = document.createElement("td");
+        td2.textContent = symbolObj.price;
+        trele.appendChild(td1);
+        trele.appendChild(td2);
+
+        changeTableByElements.appendChild(trele);
+    }
+    firstState = !firstState;
+    iterations++;
+    if (iterations < maxIterations) {
+        setTimeout(changeElementsFunction, scheduleTime);
+    } else {
+        totalEleMills = Date.now() - startEleTime;
+        startTextTime = Date.now();
+        iterations = 0;
+        setTimeout(changeTextFunction, scheduleTime);
+    }
+}
+
+// inputs and outputs for the test
+const stats = document.getElementById('stats');
+const statusDesc = document.getElementById('statusDesc');
+const iterationInput = document.getElementById('iterationInput');
 
 function starttest() {
-    // window.alert("yo " + statusDesc);
+    maxIterations = iterationInput.value;
+    window.alert("starting " + maxIterations);
 
-    while (emptyParagraph.hasChildNodes()) {
-        emptyParagraph.removeChild(emptyParagraph.lastChild);
-    }
+    startEleTime = Date.now();
+    setTimeout(changeElementsFunction, scheduleTime);
+}
 
-    statusDesc.innerText = "Finished";
+function finishtest() {
+    statusDesc.textContent = "Finished";
+    let statsContent = "changed by element milliseconds: " + totalEleMills;
+    statsContent = statsContent + " changed by text milliseconds: " + totalTextMills;
+    stats.textContent = statsContent
+    window.alert("finished " + maxIterations);
 }
 
 
