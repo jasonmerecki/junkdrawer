437d896c13a18e6750c389fe23fe76e77dbd8790
Jason Merecki <jasonmerecki@gmail.com>
2021-10-10 22:37:39 -0600

    example of classpath load config file

---
+++ ResourceLoad/src/main/java/ResourceLoad.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class ResourceLoad {
+    public static void main(String[] args)  {
+        System.out.println("Starting ResourceLoad");
+        String fileName = "myconfig.ini";
+        StringBuilder sb = new StringBuilder();
+        try (Stream<String> stream = Files.lines( Paths.get("./src/main/resources/myconfig.ini"), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> sb.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            System.out.print("IOException loading file: " + e);
+            e.printStackTrace();
+        }
+        String fileString = sb.toString();
+        System.out.println("the file loaded string: \n" + fileString);
+
+        StringBuilder textBuilder = new StringBuilder();
+        ClassLoader cl = (new ResourceLoad()).getClass().getClassLoader();
+        try (Reader ir = new InputStreamReader(cl.getResourceAsStream("fakepackage/cpconfig.ini"))) {
+            System.out.println("got the class reader " + ir);
+            try (Reader reader = new BufferedReader(ir)) {
+                int c = 0;
+                while ((c = reader.read()) != -1) {
+                    textBuilder.append((char) c);
+                }
+            } finally {
+                System.out.println("closed out the try block" );
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException reading resource: " + ioe);
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception reading resource: " + e);
+            e.printStackTrace();
+        }
+
+        System.out.println("the classpath loaded string: \n" + textBuilder);
+
+
+    }
+}
